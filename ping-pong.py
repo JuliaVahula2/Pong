3cd63465c1986a71516c65ee4055189e13cf2e20
JuliaVahula2 <136905242+JuliaVahula2@users.noreply.github.com>
2023-06-24 16:42:31 +0300

    Add Clasa GameSprite

--- ping-pong.py
+++ ping-pong.py
@@ -1,9 +1,23 @@
 from pygame import*
+
+class GameSprite(sprite.Sprite):
+     def __init__(self,player_image,player_x, player_y, pleyer_speed,width, height):
+         supper().__init__()
+         self.image=tranform.scale(image.player_image),(width, height))
+         self.rect=self.image.get_rect()
+         self.x=player_x
+         self.y=player_y
+         self.speed=player_speed
+     def reset (self):
+         mw.blit(self.image,(self.rect.x,self.rect.y))
+f
+
+
 width=600
 height=600
 
 
-mw=dissplay.set_mode((width, height))
+mw=dissplay.set_mode((width, height))p
 display.set_caption("Пін понг")
 clock=time.Clock()
 
